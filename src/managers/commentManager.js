f1bff984bccf3a0df89b802b9232ee0e0c74da57
Joshua Smith <mtgmirage@protonmail.com>
2025-02-19 10:40:57 -0600

    added a manager to commentManager.js that sends a JSON formatted new comment object to the backend API to be added to the database

--- src/managers/commentManager.js
+++ src/managers/commentManager.js
@@ -3,3 +3,13 @@ const _apiUrl = "/api/comment/"
 export const getCommentsByPostId = (postId) => {
     return fetch(_apiUrl + `${postId}`).then((res) => res.json());
 }
+
+export const postNewComment = (comment) => {
+    return fetch(_apiUrl, {
+        method: "POST",
+        headers: {
+            "Application-Type": "application/json"
+        },
+        body: comment
+    }).then((res) => res.json());
+}
